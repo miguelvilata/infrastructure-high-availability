14ec3b279d63f4027ecaad1c71d8689e0d1fabda
Reinaldo León <reinaldo.leon@smartprotection.com>
2021-07-14 22:46:58 +0200

    Demo asg ha

---
+++ asg/variables.tf
@@ -0,0 +1,56 @@
+############
+## REGION ##
+############
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "azs" {
+  default = ["eu-west-1a", "eu-west-1b"]
+  type    = list
+}
+
+##################
+## ASG CAPACITY ##
+##################
+variable "max_size" {
+  default = "1"
+}
+variable "min_size" {
+  default = "1"
+}
+variable "desired_capacity" {
+  default = "1"
+}
+variable "health_check_grace_period" {
+  default = "300"
+}
+variable "instance_type" {
+  default = "t3.small"
+}
+
+##########
+## TAGS ##
+##########
+variable "env" {
+  description = "Environment type"
+  default     = "dev"
+}
+variable "project" {
+  description = "Project name"
+  default     = "devops"
+}
+variable "creator" {
+  description = "Deploymente by"
+  default     = "DevOps Team"
+}
+variable "application" {
+  description = "Deploymente by"
+  default     = "client-server"
+}
+variable "terraform" {
+  description = "Terraform Template"
+  default     = "True"
+}
+
+
